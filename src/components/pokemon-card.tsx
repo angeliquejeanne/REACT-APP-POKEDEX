5e92b75a4b111c589ba43af32f50295601d91de8
anggiie <anggiie02>
2021-01-12 23:35:32 +0100

    add card

---
+++ src/components/pokemon-card.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent } from 'react';
+
+const PokemonCard: FunctionComponent = () => {
+    return (
+        <div>
+            Ce composant est chargé d'afficher le pokémon : ?
+        </div>
+    )
+}
+
+export default PokemonCard;
