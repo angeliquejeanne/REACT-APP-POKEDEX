0f91bfcad64a7931e89344a5375a9e842cf54746
anggiie <anggiie02>
2021-01-15 00:28:06 +0100

    crea des routes de l'app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React, {FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react';
 import PokemonList from './pages/pokemon-list';
+import PokemonDetail from './pages/pokemon-details'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 
 const App: FunctionComponent = () => {
 
 return (
-    <PokemonList />
+    <Router>
+      <div>
+        {/* La barre de navigation commune à toutes les pages */}
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className=" brand-logo center">Pokédex</Link>
+          </div>
+        </nav>
+        {/* Le système de gestion des routes de notre application */}
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route path="/pokemons/:id" component={PokemonDetail} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
   
